91952d70fd73b574d8d6207b8480f866c70e6917
Xpshnik <turnikmen2011@gmail.com>
2022-04-19 19:37:22 +0300

    CRUD implemented, minor tweaks to template

--- app.py
+++ app.py
@@ -10,11 +10,37 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
+    completed = db.Column(db.Boolean)
 
 
 db.create_all()
 
 @app.get('/')
 def todo_list():
-    return render_template('todo_list.html', todo_list=[])
+    todo_list = db.session.query(Todo).all()
+    return render_template('todo_list.html', todo_list=todo_list)
+
+
+@app.post('/add')
+def add_task():
+    task_name = request.form.get('task_name')
+    new_todo = Todo(task_name=task_name, completed=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('todo_list'))
+
+
+@app.get('/update/<int:todo_id>')
+def update_task(todo_id):
+    todo = db.sesson.query(Todo).filter(Todo.id == todo_id).first()
+    todo.completed = not todo.completed
+    db.session.commit()
+    return redirect(url_for('todo_list'))
+
+
+@app.get('/delete/<int:todo_id>')
+def delete_task(todo_id):
+    todo = db.session.query(Todo).filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('todo_list'))
